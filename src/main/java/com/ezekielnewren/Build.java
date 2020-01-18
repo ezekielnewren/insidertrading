f1b9dcf1ce0cbe93f436bf2534df1f5e7ca897d7
Ezekiel Newren <ezekielnewren@gmail.com>
2020-01-18 02:12:52 -0700

    created class Build which is a dao for build time information

---
+++ src/main/java/com/ezekielnewren/Build.java
@@ -0,0 +1,37 @@
+package com.ezekielnewren;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+public class Build {
+
+    static ImmutableMap<String, String> internal;
+
+    static {
+        Properties p = new Properties();
+        try (InputStream is = Build.class.getClassLoader().getResourceAsStream("build.info")) {
+            p.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+        for (Map.Entry<Object, Object> item: p.entrySet()) {
+            builder.put(item.getKey().toString(), item.getValue().toString());
+        }
+        internal = builder.build();
+    }
+
+
+    public static ImmutableMap<String, String> info() {
+        return internal;
+    }
+
+    public static String get(String key) {
+        return info().get(key);
+    }
+
+}

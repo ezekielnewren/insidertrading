292342bf3fd48fb9b626846ae2f52d00c8e17a1b
Ezekiel Newren <ezekielnewren@gmail.com>
2020-03-22 05:20:49 -0600

    switched to using reflection to call functions in BankAPI

--- src/main/java/com/ezekielnewren/insidertrading/BankAPI.java
+++ src/main/java/com/ezekielnewren/insidertrading/BankAPI.java
@@ -6,12 +6,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.servlet.http.HttpSession;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * This class is used by the front end for interfacing with the back.
@@ -20,52 +22,64 @@ public class BankAPI {
 
     SessionManager ctx;
 
+    Map<String, Pair<Method, JsonProperty[]>> call = new HashMap<>();
+
     public BankAPI(SessionManager _ctx) {
         ctx = _ctx;
+
+        List<Method> methList = Arrays.asList(this.getClass().getMethods());
+        for (Method m: methList) {
+
+            if (m.getParameterCount() <= 0) continue;
+            if (m.getParameterTypes()[0] != HttpSession.class) continue;
+
+            Annotation[][] annmat = m.getParameterAnnotations();
+            JsonProperty[] prop = new JsonProperty[annmat.length-1];
+            boolean good = true;
+            for (int i=0; i<prop.length; i++) {
+                if (annmat[i+1].length != 1 || annmat[i+1][0].getClass() != JsonProperty.class) {
+                    good = false;
+                    break;
+                }
+                prop[i] = (JsonProperty) annmat[i+1][0];
+            }
+            if (!good) continue;
+
+            call.put(m.getName(), new ImmutablePair<>(m, prop));
+        }
     }
 
-    String onRequest(HttpSession session, String data) throws JsonProcessingException {
-        ObjectNode json = ctx.getObjectMapper().createObjectNode();
-        String jsonOut;
-
-        String whoami = getUsername(session);
-
-        if (json.get("cmd").asText().equals("getUsername")) {
-            // no arguments for this command
-
-            // call the function
-            String result = getUsername(session);
-
-            // turn result into json
-            jsonOut = ctx.getObjectMapper().writeValueAsString(result);
-        } else if (json.get("cmd").asText().equals("getAccountList")) {
-            // no arguments for this command
-
-            // call the function
-            List<Account> result = getAccountList(session);
-
-            // turn result into json
-            jsonOut = ctx.getObjectMapper().writeValueAsString(result);
-        } else if (json.get("cmd").asText().equals("transfer")) {
-            // extract arguments from json
-            JsonNode args = json.get("args");
-            String otherUser = args.get("otherUser").asText();
-            String accountTypeFrom = args.get("accountTypeFrom").asText();
-            String accountTypeTo = args.get("accountTypeTo").asText();
-            long amount = args.get("amount").asLong();
-
-            // call the function
-            boolean result = transfer(session, otherUser, accountTypeFrom, accountTypeTo, amount);
-
-            // turn result into json
-            jsonOut = ctx.getObjectMapper().writeValueAsString(result);
-        }
-        else {
-            jsonOut = ctx.getObjectMapper().writeValueAsString(null);
+    String onRequest(HttpSession session, String data) {
+        ObjectNode response = ctx.getObjectMapper().createObjectNode();
+        JsonNode request;
+        try {
+            request = ctx.getObjectMapper().readTree(data);
+        } catch (JsonProcessingException e) {
+            response.put("error", "cannot parse request");
+            response.put("data", (String)null);
+            return response.toString();
         }
 
+        Pair<Method, JsonProperty[]> tuple = call.get(request.get("cmd").asText());
+        Method m = tuple.getLeft();
+        JsonProperty[] prop = tuple.getRight();
 
-        return jsonOut;
+        Object[] args = new Object[m.getParameterCount()];
+        args[0] = session;
+        for (int i=0; i<prop.length; i++) {
+            args[i+1] = Util.asPOJO(request.get("args").get(prop[i].value()));
+        }
+
+        try {
+            Object result = m.invoke(this, args);
+            response.put("error", (String)null);
+            response.putPOJO("data", result);
+        } catch (IllegalAccessException|InvocationTargetException e) {
+            response.put("error", e.getMessage());
+            response.putPOJO("data", null);
+        }
+
+        return response.toString();
     }
 
     public String getUsername(HttpSession session) {
